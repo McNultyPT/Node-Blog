e859777d575d2c9b56e4b436b3d6f87c8645a6a6
McNultyPT <mcnultypt@gmail.com>
2019-02-20 14:23:03 -0800

    adding .get method to get a user by id

--- data/helpers/userDb-router.js
+++ data/helpers/userDb-router.js
@@ -14,4 +14,19 @@ router.get('/', async (req, res) => {
     }
 });
 
+router.get('/:id', async (req, res) => {
+    try {
+        const user = await Users.getById(req.params.id);
+
+        if (user) {
+            res.status(200).json(user);
+        } else {
+            res.status(404).json({ message: 'That user with that ID does not exist.'});
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: 'The user could not be retrieved.'});
+    }
+});
+
 module.exports = router;
