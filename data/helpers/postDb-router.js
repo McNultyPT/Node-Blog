8a230dd0de208dd4d4842135484c5e732db82c7c
McNultyPT <mcnultypt@gmail.com>
2019-02-20 13:58:59 -0800

    adding .get method to get posts

--- data/helpers/postDb-router.js
+++ data/helpers/postDb-router.js
@@ -4,4 +4,14 @@ const Posts = require('./postDb.js');
 
 const router = express.Router();
 
+router.get('/', async (req, res) => {
+    try {
+        const posts = await Posts.get(req.query);
+        res.status(200).json(posts);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: "The posts information could not be retrieved." });
+    }
+});
+
 module.exports = router;
