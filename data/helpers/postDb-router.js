5bb4da9f12a25a364dce796377cb34abb9a2b856
McNultyPT <mcnultypt@gmail.com>
2019-02-20 15:26:09 -0800

    adding .delete method to delete a post

--- data/helpers/postDb-router.js
+++ data/helpers/postDb-router.js
@@ -32,8 +32,8 @@ router.get('/:id', async (req, res) => {
 router.post('/', async (req, res) => {
     const postInfo = req.body;
 
-    if (!postInfo.text)
-        return res.status(400).json({ errorMessage: 'Please provide text for the post.'});
+    if (!postInfo.text || !postInfo.user_id)
+        return res.status(400).json({ errorMessage: 'Please provide text and user ID for the post.'});
 
     try {
         const post = await Posts.insert(postInfo);
@@ -41,6 +41,21 @@ router.post('/', async (req, res) => {
     } catch (error) {
         console.log(error);
         res.status(500).json({ error: 'There was an error while saving this post.'});
+    }
+});
+
+router.delete('/:id', async (req, res) => {
+    const id = await Posts.remove(req.params.id);
+
+    try {
+        if (id) {
+            res.status(204).end();
+        } else {
+            res.status(404).json({ message: 'A post with that ID does not exist.'});
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: 'The post could not be removed'});
     }
 });
 
